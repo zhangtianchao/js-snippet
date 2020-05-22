153a96c2074fc971e1be7f97b5cf47ab12e963c1
zhang.tian-chao <zhang.tian-chao@inventec.com>
2020-05-22 18:00:29 +0800

    commit

--- serialport-access/jsonexec.js
+++ serialport-access/jsonexec.js
@@ -1,22 +1,26 @@
 #!/usr/bin/env node
 
-const SerialPort = require('serialport')
-const InterByteTimeout = require('./lib/parser-inter-byte-timeout')
-const port = new SerialPort('/dev/ttyUSB0')
-const parser = port.pipe(new InterByteTimeout({interval: 30}))
-parser.on('data', console.log)
-port.write('hello')
+const SerialPort = require('serialport');
+const InterByteTimeout = require('./lib/parser-inter-byte-timeout');
 
 const getopt = require('./getopt');
 
 const options = getopt([
     ['h', 'help', ''],
-    ['l', 'list', 'list himalia4 hotplug devices by native or RS232 interface'],
-    ['', 'scan=ARG', 'scan himalia4 hotplug device from ip range, ex: 192.168.0.1~192.168.0.254'],
+    ['j', 'json=ARG', 'set json file to exec'],
 ]);
 
-if (options.verbose) {
-    const verbose = parseInt(options.verbose);
-    (global as any).verbose = verbose;
+let json = null;
+
+if (options.j) {
+    const fs = require('fs');
+    json = JSON.parse(fs.readFileSync(options.j, { encoding: 'utf-8' }));
+}else{
+    console.log('please set json file to exec');
 }
 
+
+const port = new SerialPort(json.port)
+const parser = port.pipe(new InterByteTimeout({ interval: 30 }))
+parser.on('data', console.log)
+port.write('hello')
